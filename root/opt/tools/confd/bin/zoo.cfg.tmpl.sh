c1be5a4e072a4d5c2afb0e2edcd8a0e230256615
Raul Sanchez <rawmind@gmail.com>
2016-06-05 13:15:20 +0200

    Version 3.4.8-0

--- root/opt/tools/confd/bin/zoo.cfg.tmpl.sh
+++ root/opt/tools/confd/bin/zoo.cfg.tmpl.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-cat << EOF > /opt/tools/${CONF_NAME}/etc/templates/zoo.cfg.tmpl
+cat << EOF > /opt/tools/confd/etc/templates/zoo.cfg.tmpl
 tickTime=${ZK_TICK_TIME}
 initLimit=${ZK_INIT_LIMIT}
 syncLimit=${ZK_SYNC_LIMIT}
@@ -9,6 +9,6 @@ maxClientCnxns=${ZK_MAX_CLIENT_CXNS}
 clientPort=2181
 autopurge.snapRetainCount=3
 autopurge.purgeInterval=1
-{{range $i, $containerName := ls "/containers"}}
-server.{{getv (printf "/containers/%s/service_index" $containerName)}}={{getv (printf "/containers/%s/primary_ip" $containerName)}}:2888:3888{{end}}
+{{range \$i, \$containerName := ls "/containers"}}
+server.{{getv (printf "/containers/%s/service_index" \$containerName)}}={{getv (printf "/containers/%s/primary_ip" \$containerName)}}:2888:3888{{end}}
 EOF
